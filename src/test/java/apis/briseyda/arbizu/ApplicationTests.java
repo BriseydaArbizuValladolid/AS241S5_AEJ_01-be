002f42173b7bfc066c1c9e0b7a8a092d56b1332f
Briseyda Arbizu <briseyda.arbizu.valladolid@vallegrande.edu.pe>
2026-06-02 00:51:38 +0000

    feat: add a new pipeline

--- src/test/java/apis/briseyda/arbizu/ApplicationTests.java
+++ src/test/java/apis/briseyda/arbizu/ApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {MongoReactiveAutoConfiguration.class})
 class ApplicationTests {
 
 	@Test
